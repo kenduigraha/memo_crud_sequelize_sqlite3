fc9d88279908c046bcc5de6c10c8e8afb55b05b1
Ken Duigraha Putra <email": null>
2016-10-21 10:33:59 +0700

    add pagination

---
+++ public/javascripts/test.js
@@ -0,0 +1,21 @@
+//   $('#add-button').on('click', function(e){
+//     e.preventDefault()
+//
+//     var input_content = $('#content_memo').val()
+//     console.log("ftghghfghfgh");
+//     $.ajax({
+//       url: '/api/add',
+//       method: 'post',
+//       contentType: 'application/x-www-form-urlencoded',
+//       data: {
+//         content: input_content
+//       },
+//       success: function(data){
+//         console.log(data);
+//       }
+//     })
+//   })
+//
+// function test_add(){
+//
+// }
--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,57 @@ var router = express.Router();
 let models = require('../models')
 let Memos = models.Memos
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Memo' });
+router.get('/p?/:id?', function(req, res, next) {
+  let id = req.params.id
+  let set_offset = (id - 1) * 5
+  console.log(`saf`);
+  Memos.findAll({
+    offset: set_offset,
+    limit: 5
+  }).then((data_memo) => {
+    Memos.findAll().then((data_memo_page) => {
+      res.render('index', { title: 'Memo', data_memo: data_memo, data_memo_page: data_memo_page, page_id: req.params.id});
+    })
+  })
 });
 
-router.post('/', (req, res, next) => {
-  console.log(req.body.content_memo);
+// router.get('/p/:id', function(req, res, next) {
+//   let id = req.params.id
+//   let set_offset = (id - 1) * 5
+//   /*
+//   1 : 0 - 4
+//   2 : 5 - 9
+//   3 : 10 - 14
+//   */
+//   Memos.findAll({
+//     offset: set_offset,
+//     limit: 5
+//   }).then((data_memo) => {
+//     res.render('index', { title: 'Memo', data_memo: data_memo });
+//   })
+// });
+
+// router.post('/', (req, res, next) => {
+//   console.log(req.body.content_memo);
+//   Memos.create({
+//     content: req.body.content_memo
+//   }).then(() => {
+//     console.log(`Create Content Memo Success`);
+//   })
+//   res.redirect('/')
+// })
+
+router.post('/?p/:id?/add', function(req, res, next) {
+  // res.render('index', { title: 'Memo' });
+  // console.log('masuk sini');
+  // res.redirect('/test')
+  // console.log(req.body.content_memo);
   Memos.create({
-    content: req.body.content_memo
-  }).then(() => {
-    console.log(`Create Content Memo Success`);
-  })
-  res.redirect('/')
-})
+      content: req.body.content_memo
+    }).then(() => {
+      console.log(`Create Content Memo Success`);
+      res.redirect(`/p/${req.params.id}`)
+    })
+});
+
 module.exports = router;
