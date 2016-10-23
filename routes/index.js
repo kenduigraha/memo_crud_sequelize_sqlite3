d660867bd31304577f43b1721b0f87df7256e0ba
Ken Duigraha Putra <email": null>
2016-10-24 06:28:58 +0700

    changed input button to a href and order by desc

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,14 @@ var router = express.Router();
 let models = require('../models')
 let Memos = models.Memos
 /* GET home page. */
-router.get('/p/:id', function(req, res, next) {
+router.get('/p/:id/', function(req, res, next) {
   let id = req.params.id
   let set_offset = (id - 1) * 5
   // console.log(`saf`);
   Memos.findAll({
     offset: set_offset,
-    limit: 5
+    limit: 5,
+    order: 'id DESC'
   }).then((data_memo) => {
     Memos.findAll().then((data_memo_page) => {
       res.render('index', { title: 'Memo', data_memo: data_memo, data_memo_page: data_memo_page, page_id: req.params.id});
@@ -47,17 +48,28 @@ router.get('/', function(req,res){
 //   res.redirect('/')
 // })
 
-router.post('/?p/:id?/add', function(req, res, next) {
-  // res.render('index', { title: 'Memo' });
-  // console.log('masuk sini');
-  // res.redirect('/test')
-  // console.log(req.body.content_memo);
-  Memos.create({
+router.post('/p/:id/add', function(req, res, next) {
+  console.log('aadsfsd');
+  if(req.body.content_memo.length === 0){
+    let id = req.params.id
+    let set_offset = (id - 1) * 5
+    Memos.findAll({
+      offset: set_offset,
+      limit: 5,
+      order: 'id DESC'
+    }).then((data_memo) => {
+      Memos.findAll().then((data_memo_page) => {
+        res.render('index', { title: 'Memo', data_memo: data_memo, data_memo_page: data_memo_page, page_id: req.params.id, err:"Input must be filled"});
+      })
+    })
+  }else{
+    Memos.create({
       content: req.body.content_memo
     }).then(() => {
       console.log(`Create Content Memo Success`);
       res.redirect(`/p/${req.params.id}`)
     })
+  }
 });
 
 module.exports = router;
